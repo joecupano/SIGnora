4103db03455b56b236638a233a6ae9278e2b6475
Joe Cupano <joe@cupano.com>
2023-12-21 12:35:40 -0500

    Initial code

---
+++ src/SIGnora_config.h
@@ -0,0 +1,16 @@
+//
+// SIGnora_config.h
+//
+// 20231220-0100
+//
+// WiFi AP Creds
+#define WIFI_APSSID "SIGnora"
+#define WIFI_APKEY "purple"
+
+//Uncomment and edit the next two lines if connecting to existing WiFi network
+#define WIFI_SSID "HomeWAN"
+#define WIFI_KEY "2125551212"
+
+//Client credentials
+#define WWW_USER "radio"
+#define WWW_KEY "radio"
